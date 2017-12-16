65d3bb901398888aa121af1f126cb77c0b192ef8
rawmain <rawmain@gmail.com>
2017-12-17 00:13:15 +0100

    Copied from base_cm

--- custom_app.sh
+++ custom_app.sh
@@ -7,3 +7,4 @@ if [ "$apkBaseName" = "SystemUI" ];then
         echo ">>> in custom_app for $apkBaseName to adjust the field IccCardConstants$State;->NETWORK_LOCKED with IccCardConstants$State;->PERSO_LOCKED"
         find $tempSmaliDir/ -name "*.smali" | xargs sed -i 's#IccCardConstants$State;->NETWORK_LOCKED#IccCardConstants$State;->PERSO_LOCKED#g'   
 fi
+
